d8c7cb10023db727db93c2e79a3a02c7a439b51d
Atif-dija <khadijaaif2@gmail.com>
2023-10-19 06:42:42 -0700

    LIFO, FIFO

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 
-bus_t bus = {NULL, NULL, NULL, 0};
+void bus_struct(bus_t *bus);
 
 /**
  * main - the monty code interpreter
@@ -11,6 +11,7 @@ bus_t bus = {NULL, NULL, NULL, 0};
 
 int main(int argc, char *argv[])
 {
+	bus_t bus = {NULL, NULL, NULL, 0};
 	stack_t *stack = NULL;
 	char content[150];
 	FILE *file = fopen(argv[1], "r");
@@ -22,6 +23,7 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 	bus.file = file;
+	bus_struct(&bus);
 	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
@@ -30,10 +32,22 @@ int main(int argc, char *argv[])
 	while (fgets(content, sizeof(content), file) != NULL)
 	{
 		bus.content = content;
+		bus_struct(&bus);
 		counter++;
 		execute(content, &stack, counter, file);
 	}
 	stack_free(stack);
 	fclose(file);
 	return (0);
+}
+void bus_struct(bus_t *bus)
+{
+	if (bus->arg)
+		printf("%s\n", bus->arg);
+	if (bus->file)
+		fprintf(stdout, "%p\n", (void*)bus->file);
+	if (bus->content)
+		printf("%s\n", bus->content);
+	if (bus->lifi)
+		printf("%d\n", bus->lifi);
 }
